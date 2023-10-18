710590b11fda5ba50323cd90edf666a2bde098b0
sokolov pavel <sokolovpavel7@gmail.com>
2023-10-18 06:26:14 +0300

    add swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,36 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+// Initialize Swiper:
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true, // замкнутый круг прокрутки
+    parallax: true, // эффект
+    speed: 1000, //скорость прокрутки
+
+    //управление с клавиатуры
+    keyboard: {
+        enabled: true,
+    },
+
+    // Показывает, какой слайд активен (точки или цифры)
+    pagination: {
+        el: '.slider-controls__count', //селектор из нашей разметки
+        type: 'fraction' //добавление нумерации слайдеров
+    },
+
+    // Управление стрелками
+    navigation: {
+        nextEl: '#sliderNext', //id из нашей разметки
+        prevEl: '#sliderPrev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
